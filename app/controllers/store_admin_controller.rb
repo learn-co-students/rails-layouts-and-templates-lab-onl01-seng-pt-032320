f894a2d3611b83d30304548e5adbf0b965307fb9
Vlad <VladDaImpaler@gmail.com>
2020-09-01 19:00:46 -0400

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -5,6 +5,10 @@ class StoreAdminController < ApplicationController
     end
 
     def orders
-        render :layout => "order_administration"
+        render layout: "order_administration"
+    end
+
+    def invoice
+        render layout: false
     end
 end
